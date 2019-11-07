eadfb5f1812272e134f80eb0c4d021fe33409cb0
Vjeko Mistrafovic <6149@htl.rennweg.at>
2019-11-07 19:18:59 +0100

    Modified Pupil-Detection

--- demo/pupil_detection.py
+++ demo/pupil_detection.py
@@ -8,10 +8,11 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
-img = cv2.imread("augen.jpg")
+#img = cv2.imread("augen.jpg")
 fehler = cv2.imread("fehler.png")
-gray_picture2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray_picture2, 1.3, 5)
+fehlermeldung = False
+#gray_picture2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#faces = face_cascade.detectMultiScale(gray_picture2, 1.3, 5)
 
 
 detector_params = cv2.SimpleBlobDetector_Params()
@@ -20,27 +21,29 @@ detector_params.maxArea = 1500
 detector = cv2.SimpleBlobDetector_create(detector_params)
 
 def detect_eyes(img, classifier):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = eye_cascade.detectMultiScale(gray_frame, 1.3, 5)
-    width = np.size(img, 1)
-    height = np.size(img, 0)
-    left_eye = None
-    right_eye = None
-    for (x, y, w, h) in eyes:
-        if y > height / 2:
-            pass
-        eyecenter = x + w / 2
-        if eyecenter < width * 0.5:
-            left_eye = img[y:y + h, x:x + w]
+    if (img is not None):
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        eyes = eye_cascade.detectMultiScale(gray_frame, 1.3, 5)
+        width = np.size(img, 1)
+        height = np.size(img, 0)
+        left_eye = None
+        right_eye = None
+        for (x, y, w, h) in eyes:
+            if y > height / 2:
+                pass
+            eyecenter = x + w / 2
+            if eyecenter < width * 0.5:
+                left_eye = img[y:y + h, x:x + w]
+            else:
+                right_eye = img[y:y + h, x:x + w]
+
+
+        if left_eye is None:
+            print("Kein Auge erkennbar")
         else:
-            right_eye = img[y:y + h, x:x + w]
-
-
-    if left_eye is None:
-        print("Kein Auge erkennbar")
-        return fehler
+            return left_eye
     else:
-        return left_eye
+        return None
 
 
 def detect_faces(img, classifier):
@@ -61,30 +64,60 @@ def detect_faces(img, classifier):
     return frame
 
 def cut_eyebrows(img):
-    height, width = img.shape[:2]
-    eyebrow_h = int(height / 4)
-    img = img[eyebrow_h:height, 0:width]
-    if img is None:
-        print("Kein Auge erkennbar")
-        return fehler
+    if (img is not None):
+        height, width = img.shape[:2]
+        eyebrow_h = int(height / 4)
+        img = img[eyebrow_h:height, 0:width]
+        if img is None:
+            print("Kein Auge erkennbar")
+        else:
+            return img
     else:
-        return img
+        return None
 
 
 def blob_process(img, detector):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(gray_frame, 120, 255, cv2.THRESH_BINARY)
-    # img = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1) #Funktioniert noch nicht
-    cv2.imshow('img', img)
-    img = cv2.erode(img, None, iterations=2)
-    img = cv2.dilate(img, None, iterations=4)
-    img = cv2.medianBlur(img, 5)
-    keypoints = detector.detect(img)
-    if keypoints is None:
-        print("Kein Auge erkennbar")
-        return fehler
+    if (img is not None):
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, imgt = cv2.threshold(gray_frame, 60, 255, cv2.THRESH_BINARY_INV)
+        # img = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1) #Funktioniert noch nicht
+
+        imgt = cv2.erode(imgt, None, iterations=2)
+        imgt = cv2.dilate(imgt, None, iterations=4)
+        imgt = cv2.medianBlur(imgt, 5)
+        contours, _ = cv2.findContours(imgt, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        print(contours)
+        rows, cols, _ = img.shape
+        for cnt in contours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            #cv2.drawContours(img, [cnt], -1, (0,0,255), 3)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 1)
+            cv2.line(img, (x + int(w/2), 0), (x+int(w/2), rows), (0, 255, 0), 1)
+            cv2.line(img, (0, y + int(h/2)), (cols, y + int(h/2)), (0,255,0), 1)
+            break
+
+        cv2.imshow('my image', img)
+        cv2.imshow('imgt', imgt)
+        return img
     else:
-        return keypoints
+        return None
+        #keypoints = detector.detect(img)
+        #print(img.shape)
+        #for keyPoint in keypoints:
+        #print(keyPoint.pt[0])
+        #print(keyPoint.pt[1])
+        #kreis_mittelpunkt = (int(keyPoint.pt[0]), int(keyPoint.pt[1]))
+        #cv2.circle(img,kreis_mittelpunkt, 5, (255,0,0), 2  )
+        #if keyPoint.pt[1] > 16.5:
+            #print("oben")
+    #print(keypoints[0].pt[0])
+
+    #if keypoints is None:
+        #print("Kein Auge erkennbar")
+        #return fehler
+    #else:
+        #return keypoints
 
 
 cap = cv2.VideoCapture(0)
@@ -93,15 +126,19 @@ while True:
     ret, frame = cap.read()
     frame = detect_eyes(frame, eye_cascade)
     frame = cut_eyebrows(frame)
-    keypoints = blob_process(frame, detector)
-    cv2.drawKeypoints(frame, keypoints, frame, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow("my frame",frame)
+    blob_process(frame, detector)
+    if blob_process(frame, detector) is None:
+        print("NONE")
+        cv2.imshow('fehler', fehler)
+    if blob_process(frame, detector) is not None:
+        cv2.destroyWindow('fehler')
+        print("NICHT NONE")
     key = cv2.waitKey(30)
     if key == 27:
         break
 
 
-cv2.imshow('my image',img)
+#cv2.imshow('my image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
