bc768fece044ff263fefcdb07d814708081d4b06
Alexander Grübling <6138@htl.rennweg.at>
2020-03-31 15:57:50 +0200

    Implemented blinked_count switching back to zero when three seconds pass

--- demo/really_new_pupildetection.py
+++ demo/really_new_pupildetection.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import helper
+# import communicator
 import serial
 import struct
 from enum import Enum
@@ -8,7 +9,7 @@ from enum import Enum
 eye_cascade_open = cv2.CascadeClassifier('../res/eye.xml')
 eye_cascade_open_or_closed = cv2.CascadeClassifier('../res/haarcascade_righteye_2splits.xml')
 cap = cv2.VideoCapture(0)
-# ser = serial.Serial('/dev/ttyUSB0', baudrate=19200, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE)
+ser = serial.Serial('/dev/ttyUSB0', baudrate=19200, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE)
 
 is_gazing = False
 blinked_count = 0
@@ -44,17 +45,22 @@ while True:
                 print("NOTSTOPP")
                 print(notstopp_elapsed)
                 current_mode = modes.STOP
+                ser.write(struct.pack('!B', 0))
                 notstopp_start = None
 
-    
+        # Blinking detection
         if is_closed and not previously_closed:
             if blinked_count == 0: blinking_start = time.time()
             blinked_count += 1
             if blinked_count >= 3:
                 if time.time() - blinking_start <= 3:
                     current_mode = modes.DRIVE if current_mode is modes.STOP else modes.STOP
-                    # if current_mode is modes.STOP: ser.write(struct.pack('!B', 0))
+                    if current_mode is modes.STOP: ser.write(struct.pack('!B', 0))
+                blinking_start = None
                 blinked_count = 0
+        
+        if blinking_start is not None and time.time() - blinking_start >= 3:
+            blinked_count = 0
 
         previously_closed = is_closed
         
@@ -85,20 +91,20 @@ while True:
                 if distance_right < eye.shape[1] / 2.7:
                     print('right')
                     cv2.putText(frame, 'RIGHT', (50, 150), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0))
-                    # ser.write(struct.pack('!B', 1))
+                    ser.write(struct.pack('!B', 1))
                 elif distance_left < eye.shape[1] / 3:
                     print('left')
                     cv2.putText(frame, 'LEFT', (50, 150), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0))
-                    # ser.write(struct.pack('!B', 2))
+                    ser.write(struct.pack('!B', 2))
                 
                 if distance_top < eye.shape[0] / 3.5:
                     cv2.putText(frame, 'TOP', (50, 250), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0))
                     print('TOP')
-                    # ser.write(struct.pack('!B', 3))
+                    ser.write(struct.pack('!B', 3))
                 elif distance_top > eye.shape[0] / 2:
                     cv2.putText(frame, 'BOTTOM', (50, 250), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0))
                     print('BOTTOM')
-                    # ser.write(struct.pack('!B', 0))
+                    ser.write(struct.pack('!B', 0))
                 # print('Distance Right: ', distance_right, ', Distance Left: ', distance_left)
                 
                 cv2.imshow('eye', eye)
@@ -106,8 +112,8 @@ while True:
         cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     if key == 27:
-        # ser.close()
         break
 
+ser.close()
 cv2.destroyAllWindows()
 cap.release()
